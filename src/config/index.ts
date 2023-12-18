222f1e5316a2744ba47eb1c9c511114adf9e2df5
Arch7399 <raymond17399@gmail.com>
2023-12-18 16:46:36 +0530

    Responsive navbar component design

---
+++ src/config/index.ts
@@ -0,0 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: "UI Kits",
+        value: "ui-kits" as const,
+        featured: [
+            {
+                name: "dog1",
+                href: "#",
+                imageSrc: '/nav/ui-kits/dog1.jpg',
+            },
+            {
+                name: "dog2",
+                href: "#",
+                imageSrc: '/nav/ui-kits/dog2.jpg',
+            },
+            {
+                name: "dog3",
+                href: "#",
+                imageSrc: '/nav/ui-kits/dog3.jpg',
+            },
+        ]
+    },
+    {
+        label: "Icons",
+        value: "icons" as const,
+        featured: [
+            {
+                name: "cat1",
+                href: "#",
+                imageSrc: '/nav/icons/cat1.jpg',
+            },
+            {
+                name: "cat2",
+                href: "#",
+                imageSrc: '/nav/icons/cat2.jpg',
+            },
+            {
+                name: "cat3",
+                href: "#",
+                imageSrc: '/nav/icons/cat3.jpg',
+            },
+        ]
+    },
+]
