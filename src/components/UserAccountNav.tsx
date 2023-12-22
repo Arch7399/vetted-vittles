9b5f573392f43f31c907d381cefaeced2e9b0612
Arch7399 <raymond17399@gmail.com>
2023-12-22 13:55:45 +0530

    Navbar signout functionality

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,10 +1,14 @@
 'use client';
 
+import { User } from "@/payload-types";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import Link from "next/link";
+import { useAuth } from "@/hooks/use-auth";
 
 
-const UserAccountNav = () => {
+const UserAccountNav = ({user}: {user: User}) => {
+    const {signOut} = useAuth();
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild className="overflow-visible">
@@ -13,9 +17,16 @@ const UserAccountNav = () => {
             <DropdownMenuContent className="bg-white w-60" align="end">
                 <div className="flex items-center justify-start gap-2 p-2">
                     <div className="flex flex-col space-y-0.5 leading-none">
-                        <p className="font-medium text-sm text-black">user</p>
+                        <p className="font-medium text-sm text-black">{user.email}</p>
                     </div>
                 </div>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                    <Link href='/sell'>Seller dashboard</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={signOut} className="cursor-pointer">
+                    Logout
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     )
